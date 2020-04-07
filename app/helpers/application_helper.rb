d9b73edf09b1c72b9eed22f41d2616973c201c6f
shu-kob <shukobuchi+github@gmail.com>
2020-04-07 23:59:00 +0900

    Add style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def document_title
+        if @title.present?
+            "#{@title} - Blockchain Explorer"
+        else
+            "Blockchain Explorer"
+        end
+    end
 end
