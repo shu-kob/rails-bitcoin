147c31cae58659e5b8f5864977cc89d6c3f72c4a
shu-kob <shukobuchi+github@gmail.com>
2020-04-16 21:26:30 +0900

    refactoring

--- app/controllers/bitcoin_app_controller.rb
+++ app/controllers/bitcoin_app_controller.rb
@@ -182,24 +182,11 @@ class BitcoinAppController < ApplicationController
     address = params[:address]
     amount = params[:amount]
     @txid = bitcoinRPC('sendtoaddress',[address, amount])
-    @rawtx = bitcoinRPC('getrawtransaction',[@txid])
-    @txinfo = bitcoinRPC('decoderawtransaction',[@rawtx])
-    vin_address = []
-    vin_value = []
-
-    for k in 0..@txinfo['vin'].length-1
-      @vinrawtx = bitcoinRPC('getrawtransaction',[@txinfo['vin'][k]['txid']])
-      @vintx = bitcoinRPC('decoderawtransaction',[@vinrawtx])
-      @vin_outindex = @txinfo['vin'][k]['vout']
-			
-			if(@txinfo['vin'][k]['vout'])
-        @vin_address = vin_address.push(@vintx['vout'][@vin_outindex]['scriptPubKey']['addresses'][0])
-        @vin_value = vin_value.push(@vintx['vout'][@vin_outindex]['value'])
-      end
-		
-		end
-		
-		render template: 'bitcoin_app/txinfo'
+    if (@txid)
+      redirect_to txinfo_path(@txid)
+    else
+      render template: 'bitcoin_app/notfound'
+    end
   end
 
   def search
