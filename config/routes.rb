2af6d9117c0742218e30d7a87823fcd70f4b3ced
shu-kob <shukobuchi+github@gmail.com>
2020-04-16 21:30:53 +0900

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/mining', to: 'bitcoin_app#mining'
   get '/explorer', to: 'bitcoin_app#explorer'
   get '/tx/:id', to: 'bitcoin_app#txinfo', as: 'txinfo'
   get '/block/:id', to: 'bitcoin_app#blockinfo', as: 'blockinfo'
   get '/address/:id', to: 'bitcoin_app#addressinfo', as: 'addressinfo'
+  get '/mining', to: 'bitcoin_app#mining'
   get '/sendings', to: 'bitcoin_app#sendings'
   post '/sent', to: 'bitcoin_app#sent'
   get '/search', to: 'bitcoin_app#search'
-  get '/keys', to: 'bitcoin_app#keys'
   get '/notfound', to: 'bitcoin_app#notfound'
   get '/getnewaddress', to: 'bitcoin_app#getnewaddress'
   get '/wallet', to: 'bitcoin_app#wallet'
   get '/txlist', to: 'bitcoin_app#txlist'
+  get '/keys', to: 'bitcoin_app#keys'
 end
